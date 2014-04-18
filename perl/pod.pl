a25f4eebb57842f53aeb023fa950d866bded2759
warpish <warpish@gmail.com>
2014-04-18 14:53:58 -0700

    added perl/pod.pl

---
+++ perl/pod.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+=head1 Name
+
+pod.pl - POD template
+
+=head1 SYNOPSIS
+
+Synopsis
+
+=head1 DESCRIPTION
+
+Description
+
+=head1 CAVEATS
+
+Caveats
+
+=head1 COPYRIGHT
+
+Copyright
+
+=head1 AVAILABILITY
+
+Availability
+
+=head1 AUTHOR
+
+Author
+
+=head1 SEE ALSO
+
+See Also
+
+=cut
+
+=pod
+
+Yadda Yadda
+
+=cut
+
+
+print ("Usage: perldoc $0\n");
+
+exit 0;
